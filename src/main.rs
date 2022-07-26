c737751920ef001721d0b72748b90486d82511ba
Asier Illarramendi <asier@illarra.com>
2022-07-26 23:21:33 +0200

    Remove `clap` dependency and use just `std` instead

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,15 @@
-use std::path::Path;
+use std::{env, path::Path};
 
-use clap::Parser;
 use zola_rustdoc_hide_lines::formatter;
 
-#[derive(Parser)]
-#[clap(author, about, long_about = None)]
-pub struct Args {
-    // #[clap(arg_enum)]
-    // mode: Mode,
-    path: String,
-}
-
-// #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
-// pub enum Mode {
-//     Lint,
-//     Format,
-// }
-
 fn main() {
-    let args = Args::parse();
-    let path = Path::new(&args.path);
+    let args: Vec<String> = env::args().collect();
+    let folder = args.get(1);
 
-    formatter::run(path);
-
-    // match args.mode {
-    //     Mode::Lint => linter::run(path),
-    //     Mode::Format => formatter::run(path),
-    // }
+    if let Some(folder) = folder {
+        let path = Path::new(&folder);
+        formatter::run(path);
+    } else {
+        panic!("Please provide a folder.");
+    }
 }
