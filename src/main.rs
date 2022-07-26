ab99f4c1012e2328078c5aad0dd3fdbbdf5262b3
Asier Illarramendi <asier@illarra.com>
2022-07-26 17:28:45 +0200

    Remove mode selection and just leave the formatter

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
 use std::path::Path;
 
-use clap::{ArgEnum, Parser};
-use zola_rustdoc_hide_lines::{formatter, linter};
+use clap::Parser;
+use zola_rustdoc_hide_lines::formatter;
 
 #[derive(Parser)]
 #[clap(author, about, long_about = None)]
 pub struct Args {
-    #[clap(arg_enum)]
-    mode: Mode,
+    // #[clap(arg_enum)]
+    // mode: Mode,
     path: String,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
-pub enum Mode {
-    Lint,
-    Format,
-}
+// #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+// pub enum Mode {
+//     Lint,
+//     Format,
+// }
 
 fn main() {
     let args = Args::parse();
     let path = Path::new(&args.path);
 
-    match args.mode {
-        Mode::Lint => linter::run(path),
-        Mode::Format => formatter::run(path),
-    }
+    formatter::run(path);
+
+    // match args.mode {
+    //     Mode::Lint => linter::run(path),
+    //     Mode::Format => formatter::run(path),
+    // }
 }
