b659dd852e3b5ac4fb73edbca437eb3417deec52
Asier Illarramendi <asier@illarra.com>
2022-07-26 17:05:19 +0200

    Add basic formatter implementation

---
+++ src/linter.rs
@@ -0,0 +1,5 @@
+use std::path::Path;
+
+pub fn run(_dir: &Path) {
+    panic!("Linter not implemented");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::path::Path;
+
+use clap::{ArgEnum, Parser};
+use zola_rustdoc_hide_lines::{formatter, linter};
+
+#[derive(Parser)]
+#[clap(author, about, long_about = None)]
+pub struct Args {
+    #[clap(arg_enum)]
+    mode: Mode,
+    path: String,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum Mode {
+    Lint,
+    Format,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let path = Path::new(&args.path);
+
+    match args.mode {
+        Mode::Lint => linter::run(path),
+        Mode::Format => formatter::run(path),
+    }
 }
